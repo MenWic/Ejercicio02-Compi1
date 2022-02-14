6d436fb236736fe47ff87b9583d88537255d67d4
Luis Alejandro <lamr467@gmail.com>
2022-02-14 12:38:02 -0600

    Se subio el ejercicio 02 de Compiladores

---
+++ src/main/java/com/mycompany/ejercicio02compi/main/Main.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.ejercicio02compi.main;
+
+import com.mycompany.ejercicio02compi.ventanas.JframePrincipal;
+
+/**
+ *
+ * @author Luis
+ */
+public class Main {
+    public static void main(String[] args){
+        JframePrincipal window=new JframePrincipal();
+        window.setVisible(true);
+    }
+}
